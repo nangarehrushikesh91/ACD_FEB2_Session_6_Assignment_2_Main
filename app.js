067fdbae589d7759bd0978b4d99c90cd0c6cebc5
nangarehrushikesh91 <nangarehrushikesh91@gmail.com>
2017-03-11 18:48:13 +0530

    Submission of assignments
    Submission of assignments

---
+++ app.js
@@ -0,0 +1,17 @@
+var a=10,b=20,c=7;
+
+/* checks a>b and a>c  if  both conditions satisfied, A is greater */
+if (a>b  && a>c)
+{
+console.log(a+" is greater");
+}
+/* checks b>a  and b>c  if  both conditions satisfied, b is greater */
+if (b>a  && b>c)
+{
+console.log(b+" is greater");
+}
+/* if the above two conditions were false c is greater*/
+else
+{
+console.log(c+" is greater</b>");
+}
